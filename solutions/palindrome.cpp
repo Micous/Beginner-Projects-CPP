4826fa1c2104352712ee351ab4774be83d752afe
rsz <rsz.riz@gmail.com>
2015-12-27 18:06:06 +0530

    Update palindrome.cpp
    added comments

--- solutions/palindrome.cpp
+++ solutions/palindrome.cpp
@@ -5,18 +5,18 @@ int main ()
 { int num, copy, digit, rev=0;
 cout<<"Enter the number:";
 cin>>num;
-copy=num;
+copy=num; // we reverse this variable named 'copy'
 
 while (num!=0)
-{ digit = num %10; 
-rev = (rev*10) + digit;
-num = num/10;} // reverses the number
-cout<<"THe reverse of the number is:"<<rev;
+{ digit = num %10; // returns the last digit of number
+rev = (rev*10) + digit; 
+num = num/10;} // removes the last digit from number
+cout<<"The reverse of the number is:"<<rev;
 
 if(rev==copy) // compares both numbers
-cout<<"The given number is palindrome";
+cout<<"\nThe given number is palindrome";
 
 else
-cout<<"\t"<<"The given number is not a palindrome";
+cout<<"\n"<<"The given number is not a palindrome";
 return 0;
 }
